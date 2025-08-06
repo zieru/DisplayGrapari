cdd3b0e1e0da10fd2188b02aff4abbe6c063bf78
Grapari_Infomedia <zie_777@live.com>
2025-08-06 16:10:52 +0700

    new puppet

--- apps/web-antd/src/api/core/user.ts
+++ apps/web-antd/src/api/core/user.ts
@@ -16,12 +16,20 @@ export interface TVCParams {
 const transaction_id = "Q-1754299062-ID-1d0c8376-f969-47de-87be-40ad87b04853"
 const x_signature = "20911537342d5713fa6ef836b0cc35b3"
 export async function fetchWaiting(params,headers) {
-  return baseRequestClient.get('https://grapari.telkomsel.com/api-be/systemq/v1/queue/grapari-total-queue?grapari_id=9b8def0a72dc8886&status=WAITING', {
+  return baseRequestClient.get('https://grapari.telkomsel.com/api-be/systemq/v1/queue/grapari-total-queue?status=WAITING', {
     params,
     headers,
     withToken: false,
   })
 }
+
+  export async function fetchGraPARIDetail(params,headers) {
+    return baseRequestClient.get('https://grapari.telkomsel.com/api-be/systemq/v1/grapari/detail', {
+      params,
+      headers,
+      withToken: false,
+    })
+  }
 export async function fetchTvcContent(params,headers) {
   return baseRequestClient.get('https://grapari.telkomsel.com/api-be/systemq/v1/grapari/tvc-content', {
     params,
--- apps/web-antd/src/views/_core/authentication/login.vue
+++ apps/web-antd/src/views/_core/authentication/login.vue
@@ -2,20 +2,21 @@
 import type { VbenFormSchema } from '@vben/common-ui';
 import type { BasicOption } from '@vben/types';
 
-import {computed, markRaw, onMounted, reactive} from 'vue';
+import {computed, markRaw, onMounted, reactive, ref} from 'vue';
 
 import { AuthenticationLogin, SliderCaptcha, z } from '@vben/common-ui';
 import { $t } from '@vben/locales';
-import { Card } from 'ant-design-vue';
+import { Button, Card, Divider, Tag, Statistic } from 'ant-design-vue';
 import { useAuthStore } from '#/store';
-import {fetchTvcContent, fetchWaiting} from "#/api/core";
+import {fetchGraPARIDetail, fetchTvcContent, fetchWaiting} from "#/api/core";
 
 defineOptions({ name: 'Login' });
 
 const authStore = useAuthStore();
 const data = reactive({
   serving : null,
-  waiting : null
+  waiting : null,
+  detail : null
 });
 const MOCK_USER_OPTIONS: BasicOption[] = [
   {
@@ -35,6 +36,7 @@ const MOCK_USER_OPTIONS: BasicOption[] = [
 import { useSignatureStore } from '#/store/signature';
 
 const signatureStore = useSignatureStore();
+const loading = ref(true)
 
 onMounted(async () => {
   await signatureStore.fetchSignature();
@@ -45,19 +47,53 @@ onMounted(async () => {
     'api-key': 'abiXhVLnRe',
   };
 
- const result = await fetchTvcContent(
-    { grapari_id: '9b8fea0564d69c85', limit: 4 },
-    headers,
-  );
+  try {
+    loading.value = true
+    const result = await fetchTvcContent(
+      { grapari_id: '9b8fea0564d69c85', limit: 4 },
+      headers,
+    );
 
-  const result_waiting = await fetchWaiting(
-    { grapari_id: '9b8fea0564d69c85', limit: 4 },
-    headers,
-  );
-  data.serving = result.data;
-  data.waiting = result_waiting.data;
+    const result_waiting = await fetchWaiting(
+      { grapari_id: '9b8fea0564d69c85', limit: 4 },
+      headers,
+    );
+    const result_detailgra = await fetchGraPARIDetail(
+      { grapari_id: '9b8fea0564d69c85', limit: 4 },
+      headers,
+    );
+    data.serving = result.data;
+    data.waiting = result_waiting.data;
+    data.detail = result_detailgra.data;
+
+  } finally {
+    loading.value = false
+  }
+
 });
+function checkOpen(grapari) {
+  const now = new Date();
+  const currentDay = now.getDay(); // 0: Minggu, 1: Senin, ..., 6: Sabtu
+  const currentTime = now.toTimeString().slice(0, 5); // "HH:MM"
 
+  if (!grapari) return false;
+
+  let open = '';
+  let close = '';
+
+  if (currentDay === 0) {
+    open = grapari.sunday_opening_operational_hour;
+    close = grapari.sunday_closing_operational_hour;
+  } else if (currentDay === 6) {
+    open = grapari.saturday_opening_operational_hour;
+    close = grapari.saturday_closing_operational_hour;
+  } else {
+    open = grapari.weekday_opening_operational_hour;
+    close = grapari.weekday_closing_operational_hour;
+  }
+
+  return currentTime >= open && currentTime <= close;
+}
 const formSchema = computed((): VbenFormSchema[] => {
   return [
     {
@@ -121,10 +157,62 @@ const formSchema = computed((): VbenFormSchema[] => {
 
 <template>
   <div style="margin-top: 10px">
-      <Card :title="data?.serving?.grapari_name" :bordered="false" style="width: 300px">
-        <p>Serving : {{ data?.serving?.tvc_contents === null  ? 0 :  data?.serving?.tvc_contents?.length }}</p>
-        <p>Waiting : {{ data?.waiting?.total_queue_waiting.total_queue === null  ? 0 :  data?.waiting?.total_queue_waiting.total_queue }}</p>
-        <p>{{data.waiting }}</p>
-      </Card>
+    <Card
+      :title="data?.detail?.grapari_detail?.grapari_name"
+      :bordered="false"
+      style="width: 100%; max-width: 400px"
+      v-if="data?.detail?.grapari_detail?.grapari_name"
+      :loading="loading"
+    >
+      <template #extra>
+        <Tag :color="checkOpen(data?.detail?.grapari_detail) ? 'green' : 'red'">
+          {{ checkOpen(data?.detail?.grapari_detail) ? 'Buka' : 'Tutup' }}
+        </Tag>
+      </template>
+
+      <div style="display: flex; justify-content: space-between; align-items: center;">
+        <div>
+          <Statistic
+            title="Menunggu"
+            :value="data?.waiting?.total_queue_waiting?.total_queue <= 0 ? `` : data?.waiting?.total_queue_waiting?.total_queue"
+            valueStyle="color: #3ff600"
+            :suffix="data?.waiting?.total_queue_waiting?.total_queue >= 1 ? 'Pelanggan' : ``"
+          />
+        </div>
+        <div>
+          <Statistic
+            title="Sedang Dilayani"
+            v-if="data?.serving?.tvc_contents?.length >= 1"
+            :value="data?.serving?.tvc_contents?.length <= 0 ? `` : data?.serving?.tvc_contents?.length"
+            valueStyle="color: rgb(52 204 204)"
+            :suffix="data?.serving?.tvc_contents?.length >= 1 ? 'Pelanggan' : ``"
+          />
+          <span v-else> - </span>
+        </div>
+      </div>
+
+
+      <Divider dashed>
+        <strong>Alamat</strong>
+      </Divider>
+      <p>{{ data.detail?.grapari_detail?.address }}   <Button
+        :href="`https://www.google.com/maps?q=${data.detail?.grapari_detail?.latitude},${data.detail?.grapari_detail?.longitude}`"
+        target="_blank"
+        rel="noopener"
+      >
+        📍 Buka di Google Maps
+      </Button></p>
+      <Divider dashed>
+        <strong>Jam Operasional:</strong>
+      </Divider>
+      <ul>
+        <li>Senin–Jumat: {{ data.detail?.grapari_detail?.weekday_opening_operational_hour }} - {{ data.detail?.grapari_detail?.weekday_closing_operational_hour }}</li>
+        <li>Sabtu: {{ data.detail?.grapari_detail?.saturday_opening_operational_hour }} - {{ data.detail?.grapari_detail?.saturday_closing_operational_hour }}</li>
+        <li>Minggu: {{ data.detail?.grapari_detail?.sunday_opening_operational_hour }} - {{ data.detail?.grapari_detail?.sunday_closing_operational_hour }}</li>
+      </ul>
+
+    </Card>
+
+
   </div>
 </template>
